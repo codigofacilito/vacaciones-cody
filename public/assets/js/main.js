c6c10d7826c6a05d35de1f468952360fa5ec5c38
Andrés Patiño <andresfelipepatino5@gmail.com>
2021-07-23 14:21:50 -0500

    Dynamic Header

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,14 @@
+
+// Import templates
+fetch("./header.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
+
+
 // -- Sticky header
 window.onscroll = function() {
     heightFixed();
