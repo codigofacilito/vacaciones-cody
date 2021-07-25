797e266fa2200cc2b6c9f13302aeff74503c42c0
Carla Sanchez Correa <carlasanchezcorrea@gmail.com>
2021-07-25 04:59:17 -0500

    style AppBar

---
+++ src/components/StyleMaterialui/AppBar.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,   
+      ['@media (max-width:280px)']: { // eslint-disable-line no-useless-computed-key
+        fontSize: '14px'      
+      }
+    },
+    subtitle: {
+        ['@media (max-width:280px)']: { // eslint-disable-line no-useless-computed-key
+          fontSize: '12px'      
+        }
+      },  
+}));
+export { useStyles }
