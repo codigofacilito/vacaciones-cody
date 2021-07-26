d967b3dd5f8fa8c13753f1cb165775550055db09
silviaespanagil <silviaespanagil@gmail.com>
2021-07-26 13:50:55 +0200

    Add header component and basic styles for it

--- cody/src/components/App.js
+++ cody/src/components/App.js
@@ -1,22 +1,15 @@
+//COMPONENTS
+import Header from "./1 - Header";
+
+//STYLESHEET
 import "../stylesheets/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <Header />
+    </>
   );
 }
 
