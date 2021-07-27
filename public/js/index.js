dbd38b8e786766eca96763848b58903bf1454520
Mary Rojas <mary.rojas@square1.io>
2021-07-27 10:20:57 -0400

    Add Tailwind and Alpine - Create sections

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('border-b-2 border-secondary');
+        })
+        $(this).addClass('border-b-2 border-secondary');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            currLink.addClass("border-b-2 border-secondary");
+        }
+        else{
+            currLink.removeClass("border-b-2 border-secondary");
+        }
+    });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6e57fc',
+        secondary: '#A596FE',
+        terciary: '#fd585f',
+      },
+      fontFamily: {
+        'sans': ['"Inter"', 'sans-serif'],
+        'serif': ['"Playfair Display"', 'serif']
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
