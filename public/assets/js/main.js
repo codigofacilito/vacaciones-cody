1fd083289b5b8815db25758a3658543526db3d97
Andrés Patiño <andresfelipepatino5@gmail.com>
2021-07-27 12:02:07 -0500

    Footer

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,12 @@
 
 // Import templates
 fetch("./header.html")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        document.querySelector("header").innerHTML = data;
-    });
+    .then(response => { return response.text() })
+    .then(data => { document.querySelector("header").innerHTML = data })
+
+fetch("./footer.html")
+    .then(response => { return response.text() })
+    .then(data => { document.querySelector("footer").innerHTML = data })
 
 
 // -- Sticky header
