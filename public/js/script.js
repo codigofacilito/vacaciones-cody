26df699c7534fe28b74aa34b99bbadc4b0802638
ManuelLpz <manuel@mztweb.com>
2021-07-26 19:03:23 -0600

    Cambio Estilos y Fondo Mas Relacionado A Tematica

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,32 @@
 let counter = 1;
 let slider_container = document.querySelector('.slider_container');
+let itemImg = document.querySelector('#item');
 
 
-const setStyles = (element,styles) => {
-    Object.assign(element.style,styles);
-}
+// const setStyles = (element,styles) => {
+//     Object.assign(element.style,styles);
+// }
+
+setInterval(() => {
+    counter++;
+    if(counter>6)
+        counter=1;
+        
+    itemImg.src = `images/${counter}.jpg`;
+}, 10000);
 
 const next_img = () => {
     counter++;
     if(counter>6)
         counter=1;
-        
-    setStyles(slider_container,{
-        'background':`url(images/${counter}.jpg)`,
-        'backgroundSize':'cover'
-    })
+
+    itemImg.src = `images/${counter}.jpg`;
 }
 
 const previous_img = () => {
     counter--;
     if(counter<1)
         counter=6;
-        
-    setStyles(slider_container,{
-        'background':`url(images/${counter}.jpg)`,
-        'backgroundSize':'cover'
-    })
+    
+    itemImg.src = `images/${counter}.jpg`;
 }
