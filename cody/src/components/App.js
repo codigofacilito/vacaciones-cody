1529a986eb0c7cce1b0fab38f1f02dd937981739
silviaespanagil <silviaespanagil@gmail.com>
2021-07-28 13:40:53 +0200

    Add styles to carousel and breakpoints width

--- cody/src/components/App.js
+++ cody/src/components/App.js
@@ -2,6 +2,7 @@
 import Header from "./1 - Header";
 import Presentation from "./2 - Presentation";
 import Studies from "./3 - Studies";
+import Carousel from "./5-Carousel";
 
 //STYLESHEET
 import "../stylesheets/App.scss";
@@ -13,6 +14,7 @@ function App() {
       <main>
         <Presentation />
         <Studies />
+        <Carousel />
       </main>
     </>
   );
