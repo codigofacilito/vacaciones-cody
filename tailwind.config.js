cf858d8577c5f14071935c0e73e1a5812932d81c
NiverMtz <nivermartinezmed@gmail.com>
2021-07-29 06:40:09 -0500

    first styles navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     },
     extend: {
       colors: {
-        danger: '#C400FF',
+        summer: {
+          100 : '#C400FF',
+          200 : '#FF67E7',
+          300 : '#FFF338',
+          400 : '#0CECDD'
+        },
         cian: {
           100 : '#1E5F74',
           200 : '#133B5C',
