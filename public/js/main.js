7ffd16299ae8eef7518ee52c59e2696304020fe7
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-07-28 19:49:11 -0500

    updated website to make it responsive and navigation var was fixed

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 const renderFotos = () => {
   const fotosview = document.getElementById("fotos-view");
   document.getElementById("app").innerHTML = fotosview.innerHTML;
-  const fotosbtn = document.getElementById("main-btn");
+  const fotosbtn = document.getElementById("inicio");
   fotosbtn.addEventListener("click", (e) => {
     e.preventDefault();
     rendermain();
@@ -11,7 +11,7 @@ const renderFotos = () => {
 const rendermain = () => {
   const mainview = document.getElementById("main-view");
   document.getElementById("app").innerHTML = mainview.innerHTML;
-  const fotosbtn = document.getElementById("fotos-btn");
+  const fotosbtn = document.getElementById("fotos");
   fotosbtn.addEventListener("click", (e) => {
     e.preventDefault();
     renderFotos();
