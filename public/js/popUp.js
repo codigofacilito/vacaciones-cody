c161b75fc69bf90ed970f8ebfb56c8df23d69668
Angel Berlanga <angel.tigre@hotmail.com>
2021-07-28 20:46:27 -0500

    Crear estilos para la galería
    Implementaciones:
    - Carga asincrónica de estilos
    - Optimizar fotos.html para favorecer rendimiento
    - Cambiar imágenes a .webp y comprimirlas
    - Optimizar para SEO
    - Agregar rel="noreferrer" a links de recursos externos
    
    Cambios:
    - Eliminar los elementos no utilizados de reset.css
    - Cambiar color accent favoreciendo contrast ratio
    - Eliminar CSS Variables no utilizadas
    - Reestructurar estilos y scripts en archivos separados para cargar solo
      los necesarios para la página actual
    - Especificar estilos de footer para evitar sobreposición de utils.css
    - Igualar el head de index.html y fotos.html

---
+++ public/js/popUp.js
@@ -0,0 +1,62 @@
+let popUpImages = document.querySelectorAll('.Gallery-foto')
+let popUpImage = document.createElement('img')
+let popUpCaption = document.createElement('span')
+let popUpCloseIcon = document.createElement('img')
+let popUpBackground = document.createElement('div')
+let popUpContainer = document.createElement('div')
+
+let showingPopUp = false
+
+popUpImages.forEach((imageElement, index)=> {
+  imageElement.addEventListener('click', e => {
+    let imageIndex = `image-${index}`
+
+    // DUPLICATE IMAGE
+    let image = e.target
+    popUpImage.classList.add('u-rounded')
+    popUpImage.classList.add('PopUp-image')
+    popUpImage.setAttribute('src', image.src)
+
+    // CREATE IMAGE CAPTION
+    popUpCaption.classList.add('PopUp-caption')
+    popUpCaption.classList.add('u-paragraph')
+    popUpCaption.innerText = image.alt
+
+    // CREATE CLOSE ICON
+    popUpCloseIcon.classList.add('PopUp-close')
+    popUpCloseIcon.src = '/images/icons/slide-down.png'
+     
+    // CREATE BACKGROUND
+    popUpBackground.classList.add('PopUp-background')
+
+    // CREATE CONTAINER
+    popUpContainer.classList.add('PopUp')
+    popUpContainer.id = imageIndex
+
+    if (showingPopUp && !e.target.matches('.PopUp-background')) return
+    renderPopUp()
+
+    showingPopUp = true
+  })
+})
+
+function renderPopUp() {
+  document.body.appendChild(popUpBackground)
+  popUpContainer.appendChild(popUpCloseIcon)
+  popUpContainer.appendChild(popUpImage)
+  popUpContainer.appendChild(popUpCaption)
+  document.body.appendChild(popUpContainer)
+}
+
+popUpBackground.addEventListener('click', closePopUp)
+popUpCloseIcon.addEventListener('click', closePopUp)
+  
+function closePopUp() {
+  popUpContainer.classList.add('PopUp-hide')
+  setTimeout(() => {
+    document.body.removeChild(popUpContainer)
+    popUpContainer.classList.remove('PopUp-hide')
+    document.body.removeChild(popUpBackground)
+  }, 750)
+  showingPopUp = false
+}
