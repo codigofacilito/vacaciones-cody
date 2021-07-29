6ace30e42543b29fd131d869189fe5969f839e00
NiverMtz <nivermartinezmed@gmail.com>
2021-07-29 05:50:43 -0500

    code navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
+  mode: 'jit',
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      rale: ['Raleway', 'sans-serif'],
+      roboto: ['Roboto', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        danger: '#C400FF',
+        cian: {
+          100 : '#1E5F74',
+          200 : '#133B5C',
+          300 : '#1D2D51'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
