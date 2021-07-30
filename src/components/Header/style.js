32f3a0ed8950f1442e967217ea679d34ce53888f
HectorMtz22 <hec.gre81@gmail.com>
2021-07-30 01:06:00 -0500

    Renewed Header

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
+import theme from "theme";
 
 export const Header = styled.header`
-  padding: 1rem 5%;
+  padding: 1rem 5vw;
+  color: #fff;
+  background-color: ${theme.colors.primary};
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
 `;
