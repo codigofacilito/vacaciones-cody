1ffb8744fce9f7e6350a64abd8e9a132d9b19214
HectorMtz22 <hec.gre81@gmail.com>
2021-07-30 00:33:03 -0500

    Theme for colors

---
+++ theme/index.js
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    primary: "#243173",
+    primaryVariant: "#2841BF",
+    secondary: "#73402F",
+    third: "#417318",
+    thirdVariant: "#72BF32",
+  },
+};
+
+export default theme;
