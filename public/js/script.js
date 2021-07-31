94f4edb9e4b9e4b743a02939486bcf6310827510
ManuelLpz <manuel@mztweb.com>
2021-07-30 18:26:46 -0600

    Se Anadieron Efectos En Seccion De Fotos En Archivo Index

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,37 @@
 let counter = 1;
 let slider_container = document.querySelector('.slider_container');
 
-setInterval(() => {
-    counter++;
-    if(counter>6)
-        counter=1;
-        
-    setStyles(slider_container,{
-        'background':`url('images/${counter}.jpg')`,
-        'background-size':'cover'
-    })
-}, 5000);
+const showModal = (itemImg) => {
+    const fotos_container = document.querySelector('.fotos');
+
+    const containerModal = document.createElement('div');
+    const imgModal = document.createElement('img');
+    const titleImg = document.createElement('h1');
+
+
+    containerModal.classList.add('container_modal');
+    fotos_container.appendChild(containerModal);
+
+    titleImg.classList.add('title_img');
+    titleImg.innerHTML = itemImg.alt;
+    imgModal.src = itemImg.src;
+    imgModal.classList.add('img_modal');
+
+    containerModal.innerHTML = `<span class="icon_exit" onclick="exitModal()"><i class="fas fa-times-circle"></i></span><br>`;
+    containerModal.appendChild(titleImg);
+    containerModal.appendChild(imgModal);
+    
+}
+
+const exitModal = () => {
+    let container = document.querySelector('.container_modal');
+    document.querySelector('.fotos').removeChild(container);
+}
+
+const showCompleteImg = e => {
+    let itemImg = e.target;
+    showModal(itemImg);    
+}
 
 const setStyles = (element,styles) => {
     Object.assign(element.style,styles);
