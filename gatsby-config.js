e80df9ccf8f3bc9d17c18c1d58be787d249d4eed
HectorMtz22 <hec.gre81@gmail.com>
2021-08-01 00:38:38 -0500

    React Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,6 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    `gatsby-plugin-react-helmet`,
   ],
 };
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,11 +4,13 @@ import Header from "components/Header";
 import Footer from "components/Footer";
 import Contact from "components/Contact";
 import { GlobalStyle, Layout } from "./style";
+import { Helmet } from "react-helmet";
 import theme from "theme";
 
 export default function LayoutComponent({
   children,
   title = "Mis vacaciones",
+  description = "Esta es una pagina para el concurso de CodigoFacilito",
 }) {
   const [height, setHeight] = useState(null);
   const headerRef = useRef(null);
@@ -20,18 +22,29 @@ export default function LayoutComponent({
   return (
     <>
       <GlobalStyle />
-      <title>{title} - HectorMtz22</title>
-      <meta name="theme-color" content={theme.colors.primaryVariant} />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="true"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Raleway&display=swap"
-        rel="stylesheet"
-      />
+      <Helmet
+        htmlAttributes={{
+          lang: "es",
+        }}
+      >
+        <title>{title} - HectorMtz22</title>
+        <meta name="theme-color" content={theme.colors.primaryVariant} />
+        <meta name="description" content={description} />
+        <meta
+          name="keywords"
+          content="concurso, codigo facilito, hmtzdev, hectormtz22"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Header ref={headerRef} />
       <Sidebar height={height} />
       <Layout>
