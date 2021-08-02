89fcccca04c7b8428faaae4dd83415f9c4c773b5
Sebastian GC <sebastiangzzcor@gmail.com>
2021-08-02 14:58:49 -0500

    Avances del proyecto.

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const Container = document.getElementById('Container'),
+	PageGallery = document.getElementById('gallery'),
+	Buttons = Array.from(document.querySelectorAll('#openGallery'));
+
+Buttons.map((button) => button.addEventListener('click', OpenGallery));
+PageGallery.addEventListener('click', CloseGallery);
+
+function OpenGallery() {
+	PageGallery.classList.add('open');
+	Container.classList.add('hidden');
+}
+
+function CloseGallery() {
+	PageGallery.classList.remove('open');
+	Container.classList.remove('hidden');
+}
