cc2d97b6fb9bb7e9704fe27bceeda346ebd7dde1
Angel Berlanga <angel.tigre@hotmail.com>
2021-08-01 20:25:23 -0500

    Reajustar configuración para PWA

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ getViewportHeight()
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("../serviceWorker.js")
         .then(_ => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
   })
