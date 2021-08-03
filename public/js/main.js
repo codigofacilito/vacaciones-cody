73cd4da695273da2318b5b24ce27ccafe80cc994
Jussefo91 <jussef.zamorano@spira.com.mx>
2021-08-03 12:09:19 -0500

    ajustes de css

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
   var instances = M.Sidenav.init(elems);
+  var elems = document.querySelectorAll(".parallax");
+  var instances = M.Parallax.init(elems);
 });
 var app = document.getElementById("app");
 
@@ -14,10 +16,31 @@ var controller = new ScrollMagic.Controller();
 
 // create a scene
 // .setPin("#escena1")
-var scene = new ScrollMagic.Scene({ triggerElement: "#escena1", duration: 400 })
-  .offset(0)
+var scene1 = new ScrollMagic.Scene({ triggerElement: "#escena1", duration: 370 })
   .on("progress", function (e) {
-    console.log(e.progress);
+    document.getElementById("pin1").style.opacity = e.progress;
   })
   .addIndicators({ name: "escena 1" }) // add indicators (requires plugin)
+  .addTo(controller);
+var scene2 = new ScrollMagic.Scene({ triggerElement: "#escena2", duration: 400 })
+  .on("progress", function (e) {
+    document.getElementById("pin2").style.opacity = e.progress;
+  })
+  .on("enter leave", function (e) {
+    // elemento = document.getElementById("pin2");
+    // if (e.type == "enter") {
+    //   anime({
+    //     targets: elemento,
+    //     opacity: 1,
+    //     duration: 1800,
+    //   });
+    // } else {
+    //   anime({
+    //     targets: elemento,
+    //     opacity: 0,
+    //     duration: 1800,
+    //   });
+    // }
+  })
+  .addIndicators({ name: "escena 2" }) // add indicators (requires plugin)
   .addTo(controller);
