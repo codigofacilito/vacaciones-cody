6c68aab2fee26f118fd3cfae77ce3fac6eb5dcb2
ErickCapilla <erickpoblano25@gmail.com>
2021-08-03 00:13:44 -0500

    Navbar done

--- public/script/main.js
+++ public/script/main.js
@@ -1,13 +1,22 @@
-const darkMode = document.getElementById('mode_check');
+// Functions
+function changeMode() {
+  document.body.style.transition = 'all 3s'
 
+  darkMode.checked ? document.documentElement.style.setProperty('--primary-color', '#050036')
+  : document.documentElement.style.setProperty('--primary-color', '#0092d1')
+}
+
+window.addEventListener('load', () => {
+  changeMode()
+})
+
+const darkMode = document.getElementById('mode_check');
 
 darkMode.addEventListener('click', () => {
   const circle = document.querySelector('.mode-circle');
 
-    circle.style.transition = 'all 2s'
-    circle.classList.toggle('active')
-
-    if(darkMode.checked) {
-      console.log(true);
-    }
+  circle.style.transition = 'all 2s'
+  circle.classList.toggle('active')
+  changeMode()
 });
+
