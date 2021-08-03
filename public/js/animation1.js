7fc095092f7b6756db0a79d23ea3c1011cd8c5fe
carlosf <carlos@mail.com>
2021-08-03 15:15:46 -0500

    arreglo de dos secciones mas anexo del boton ir arriba

--- public/js/animation1.js
+++ public/js/animation1.js
@@ -1,2 +1,15 @@
 document.querySelector("li:nth-child(1) path").getTotalLength();
 
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: '0px' }, 300); 
+        return false; 
+    }); 
+});
