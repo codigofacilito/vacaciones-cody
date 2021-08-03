183be423c87808b75a54c680acb43d9f32bdc2ee
formacioncivica3 <79419192+formacioncivica3@users.noreply.github.com>
2021-08-03 15:21:33 -0500

    Delete public directory

--- public/main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-  });
-  
-  ScrollReveal().reveal('.showcase');
-  ScrollReveal().reveal('.news-cards', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
