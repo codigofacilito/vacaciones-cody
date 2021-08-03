9290c032c193f430d196bf2e24b2169015787a5c
formacioncivica3 <79419192+formacioncivica3@users.noreply.github.com>
2021-08-03 15:19:37 -0500

    Add files via upload

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>💻 Developed by David Mora using VueJS 💚</footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+$hue: 209;
+$saturation: 77%;
+$lightness: 4%;
+$font-color: hsl(203, 38%, 90%);
+
+footer {
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    bottom: 0;
+    width: 100%;
+    color: $font-color;
+    font-weight: bold;
+    font-family: 'IBM Plex Sans', sans-serif;
+    background-color: hsl($hue, $saturation, $lightness);
+}
+</style>
