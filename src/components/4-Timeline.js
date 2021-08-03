4532c8f5a3a9a2eb6b8f82d3c4f6363d4f7fbd8a
silviaespanagil <silviaespanagil@gmail.com>
2021-08-03 13:06:13 +0200

    Move to main folder

---
+++ src/components/4-Timeline.js
@@ -0,0 +1,26 @@
+import learningPath from "../data/learningPath.json";
+
+const Timeline = () => {
+  //RENDER FUNCTION OF LEARNING PATH
+  const willDo = learningPath.map((data) => {
+    return (
+      <li className="path__timeline--container" key={data.id}>
+        {data.tech}
+      </li>
+    );
+  });
+  if (learningPath.length > 0)
+    return (
+      <>
+        <article className="path">
+          <h3 className="path__title">Ruta de aprendizaje 2021</h3>
+          <p className="path__paragraph">
+            Este año me propuse aprender estas nuevas tecnologías🚀
+          </p>
+          <ul className="path__timeline">{willDo}</ul>
+        </article>
+      </>
+    );
+};
+
+export default Timeline;
---
+++ src/components/5-Carousel.js
@@ -0,0 +1,58 @@
+//COMPONENTS
+import ImageGallery from "react-image-gallery";
+
+const Carousel = () => {
+  const images = [
+    {
+      original: "https://i.ibb.co/nBy6GSb/1.jpg",
+      thumbnail: "https://i.ibb.co/nBy6GSb/1.jpg",
+      originalAlt: "Bici en la playa",
+      description:
+        "Este soy yo montando bicicleta en la playa ⛱️🌴. Recuerden utilizar  protector solar🌞",
+    },
+    {
+      original: "https://i.ibb.co/wJbHty1/2.jpg",
+      thumbnail: "https://i.ibb.co/wJbHty1/2.jpg",
+      originalAlt: "Campamento  en las Cies",
+      description: "Acampando en las Cies, ¡el agua está fría!❄️",
+    },
+    {
+      original: "https://i.ibb.co/NWQxmV8/3.jpg",
+      thumbnail: "https://i.ibb.co/NWQxmV8/3.jpg",
+      originalAlt: "Momento de relax en la piscina",
+      description:
+        "Nada como una siesta en la piscina 💤, ¿les gusta mi flotador?",
+    },
+
+    {
+      original: "https://i.ibb.co/q7rmJHW/4.jpg",
+      thumbnail: "https://i.ibb.co/q7rmJHW/4.jpg",
+      originalAlt: "Atardecer en la playa",
+      description: "Atardecer en la playa + agua de coco = 💗",
+    },
+    {
+      original: "https://i.ibb.co/LtczDky/5.jpg",
+      thumbnail: "https://i.ibb.co/LtczDky/5.jpg",
+      originalAlt: "Vespa Tour",
+      description: "Me  fui de Tour con la Vespa 🛵, ¡la pasé genial!",
+    },
+    {
+      original: "https://i.ibb.co/W2VZj1v/6.jpg",
+      thumbnail: "https://i.ibb.co/W2VZj1v/6.jpg",
+      originalAlt: "En el aeropuerto",
+      description: "El tiempo vuela y yo también, ¡ese es mi avión!✈️🛫",
+    },
+  ];
+
+  return (
+    <>
+      <h3 className="pictures__title" id="vacations">
+        Estas son algunas fotos de mis vacaciones
+      </h3>
+      <div className="carousel">
+        <ImageGallery items={images} />
+      </div>
+    </>
+  );
+};
+export default Carousel;
