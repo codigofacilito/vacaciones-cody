6861c3ad19989466aa18d325696b9397f91cfbf5
ErickCapilla <erickpoblano25@gmail.com>
2021-08-03 17:39:12 -0500

    Added a scrollbar view

--- public/script/main.js
+++ public/script/main.js
@@ -1,10 +1,33 @@
 // Functions
 function changeMode() {
   document.body.style.transition = 'all 3s'
+  const sunMoon = document.getElementById('sunMoon');
 
-  darkMode.checked ? document.documentElement.style.setProperty('--primary-color', '#050036')
-  : document.documentElement.style.setProperty('--primary-color', '#0092d1')
+  if(darkMode.checked) {
+    document.documentElement.style.setProperty('--primary-color', '#050036')
+    sunMoon.style.backgroundImage = "url('https://img.icons8.com/office/96/000000/moon.png')"
+  } else {
+    document.documentElement.style.setProperty('--primary-color', '#0092d1')
+    sunMoon.style.backgroundImage = "url('https://img.icons8.com/fluency/96/000000/sun.png')"
+  }
 }
+
+function scrollBar() {
+  const sunMoon = document.getElementById('sunMoon');
+  const scrollBar = document.querySelector('.scroll');
+
+
+  window.addEventListener('scroll', e => {
+    let scrollValue = e.path[1].scrollY;
+    let scrollHeight = document.documentElement.scrollHeight;
+    let scroll = ((scrollBar.clientHeight / scrollHeight) * 100)
+    let result = ((scroll * scrollValue) / 100) * 1.5;
+
+    sunMoon.style.top = String(Math.floor(result - 8)) + 'px';
+  })
+}
+
+scrollBar()
 
 window.addEventListener('load', () => {
   document.body.style.transition = ''
@@ -20,4 +43,5 @@ darkMode.addEventListener('click', () => {
   circle.classList.toggle('active')
   changeMode()
 });
+
 
