56fba41009481133520934f3a84e6a231b2358b9
servicios-terceros <51757797+servicios-terceros@users.noreply.github.com>
2021-08-03 19:53:29 -0600

    cambios en el index y fotos

---
+++ public/js/swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    slidesPerGroup: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
