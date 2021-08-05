5ac5017d0cba1a8418b44b65e79c1c5b1e3eebb8
Juan Pedro Sosa <juanpedrososa@outlook.com>
2021-08-05 20:09:00 -0300

    la imagenes declaradas en elements.js no se veian, corrijo el path

--- public/javascript/slider/elements.js
+++ public/javascript/slider/elements.js
@@ -2,25 +2,25 @@ const elements = [
 	{
 		subtitle: "La importancia de las vacaciones",
 		title: "Salud",
-		image: "../public/images/undraw_junior_soccer_6sop.svg",
+		image: "images/undraw_junior_soccer_6sop.svg",
 		text: "Mejora la salud mental"
 	},
 	{
 		subtitle: "La importancia de las vacaciones",
 		title: "Bienestar",
-		image: "../public/images/undraw_A_day_at_the_park_re_9kxj.svg",
+		image: "images/undraw_A_day_at_the_park_re_9kxj.svg",
 		text: "Incrementa la motivación"
 	},
 	{
 		subtitle: "La importancia de las vacaciones",
 		title: "Calidad de vida",
-		image: "../public/images/undraw_healthy_lifestyle_6tyl.svg",
+		image: "images/undraw_healthy_lifestyle_6tyl.svg",
 		text: "Reduce el burnout"
 	},
 	{
 		subtitle: "La importancia de las vacaciones",
 		title: "Rendimiento",
-		image: "../public/images/undraw_inspiration_lecl.svg",
+		image: "images/undraw_inspiration_lecl.svg",
 		text: "Mejora tu productividad y creatividad"
 	}
 ];
