86c045a8b96eb5b794f1c23ee3c5c9bb129020cf
Xibalsba <jl573160@gmail.com>
2021-08-06 00:13:58 -0500

    Desplegar imagen de cada storie

--- public/js/main.js
+++ public/js/main.js
@@ -76,12 +76,12 @@ var main = new Vue({
                 icon:"images/profile.jpg",
                 content: [
                   {
-                    picture: "images/atardecerplaya.jpg",
+                    picture: "https://cdn.pixabay.com/photo/2018/06/21/14/00/swimming-pool-3488634__340.jpg",
                     message: "Durante el verano de 2019, decidí comenzar a aprender desarrollo frontend, por lo que durante mi viaje, estuve aprendiendo HTML, CSS, y JavaScript",
                     emojis: '<i class="em em-nerd_face" aria-role="presentation" aria-label="NERD FACE"></i>'
                   },
                   {
-                    picture: "images/atardecerplaya.jpg",
+                    picture: "https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712__340.jpg",
                     message: "Cada tarde, después de conocer nuevos lugares en mis vacaciones, practicaba mis habilidades en tecnologías frontend, construyendo esta página",
                     emojis: '<i class="em em-pencil2" aria-role="presentation" aria-label="PENCIL"></i>'
                   }
@@ -92,32 +92,32 @@ var main = new Vue({
                 icon:"images/profile.jpg",
                 content: [
                   { 
-                    picture: "images/codigo.jpg",
+                    picture: "https://cdn.pixabay.com/photo/2016/03/09/09/17/desktop-1245714__340.jpg",
                     message: "También me propuse aprender nuevas tecnologías durante 2021",
                     emojis: '<i class="em em-book" aria-role="presentation" aria-label="OPEN BOOK"></i>'
                   },
                   {
-                    picture: "images/codigo.jpg",
+                    picture: "https://cdn.pixabay.com/photo/2020/04/30/15/34/code-5113374__340.jpg",
                     message: "Flexbox, y Grid",
                     emojis: '<i class="em em-notebook" aria-role="presentation" aria-label="NOTEBOOK"></i>'
                   },
                   {
-                    picture: "images/codigo.jpg",
+                    picture: "https://cdn.pixabay.com/photo/2017/08/01/22/38/flash-2568381__340.jpg",
                     message: "Animaciones con CSS y JavaScript",
                     emojis: '<i class="em em-octopus" aria-role="presentation" aria-label="OCTOPUS"></i>'
                   },
                   {
-                    picture: "images/codigo.jpg",
+                    picture: "https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536__340.jpg",
                     message: "AJAX",
                     emojis: '<i class="em em-outbox_tray" aria-role="presentation" aria-label="OUTBOX TRAY"></i>'
                   },
                   {
-                    picture: "images/codigo.jpg",
+                    picture: "https://cdn.pixabay.com/photo/2015/09/17/17/25/code-944499__340.jpg",
                     message: "HTML semántico",
                     emojis: '<i class="em em-page_facing_up" aria-role="presentation" aria-label="PAGE FACING UP"></i>'
                   },
                   {
-                    picture: "images/codigo.jpg",
+                    picture: "https://cdn.pixabay.com/photo/2018/07/15/10/44/dna-3539309__340.jpg",
                     message: "Entre otros",
                     emojis: '<i class="em em-nerd_face" aria-role="presentation" aria-label="NERD FACE"></i>'
                   }
@@ -128,32 +128,32 @@ var main = new Vue({
                 icon:"images/profile.jpg",
                 content: [
                   {
-                    picture: "images/videojuegos.png",
+                    picture: "https://cdn.pixabay.com/photo/2020/01/24/12/29/konigssee-4790116__340.jpg",
                     message: "Aprender a programar y escribir código, puede ser estresante, por eso es muy importante que periódicamente nos desconectemos y descansemos apropiadamente",
                     emojis: '<i class="em em-beach_with_umbrella" aria-role="presentation" aria-label=""></i>'
                   },
                   {
-                    picture: "images/videojuegos.png",
+                    picture: "https://cdn.pixabay.com/photo/2017/09/06/22/21/vw-2723353__340.jpg",
                     message: "Durante mis vacaciones aprendí que existen distintos beneficios de descansar:",
                     emojis: ''
                   },
                   {
-                    picture: "images/videojuegos.png",
+                    picture: "https://cdn.pixabay.com/photo/2017/11/11/21/08/paint-2940513__480.jpg",
                     message: "Mejora la salud mental",
                     emojis: '<i class="em em-brain" aria-role="presentation" aria-label="BRAIN"></i>'
                   },
                   {
-                    picture: "images/videojuegos.png",
+                    picture: "https://cdn.pixabay.com/photo/2019/08/07/08/33/sunset-4390237__340.jpg",
                     message: "Incrementa la motivación",
                     emojis: '<i class="em em-first_place_medal" aria-role="presentation" aria-label="FIRST PLACE MEDAL"></i>'
                   },
                   {
-                    picture: "images/videojuegos.png",
+                    picture: "https://cdn.pixabay.com/photo/2016/08/23/13/12/knitting-1614283__340.jpg",
                     message: "Reduce el burnout(desgaste profesional)",
                     emojis: '<i class="em em-dizzy_face" aria-role="presentation" aria-label="DIZZY FACE"></i>'
                   },
                   {
-                    picture: "images/videojuegos.png",
+                    picture: "https://cdn.pixabay.com/photo/2017/11/11/21/08/paint-2940513__480.jpg",
                     message: "Mejora tu productividad y creatividad",
                     emojis: '<i class="em em-rocket" aria-role="presentation" aria-label="ROCKET"></i>'
                   }
