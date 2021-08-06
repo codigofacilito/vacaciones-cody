0876d433f99a7c2d9f8aa7a2b27c974bcc60beaf
Xibalsba <jl573160@gmail.com>
2021-08-06 00:19:26 -0500

    Fijar la cantidad de comentarios y likes de cada post

--- public/js/main.js
+++ public/js/main.js
@@ -9,8 +9,8 @@ var main = new Vue({
                   picture: "images/profile.jpg"
                 },
                 description: "Dando una vuelta en bicicleta en la playa!",
-                comments: 0,
-                likes: 0,
+                comments: 123,
+                likes: 400,
                 emojis: '<i class="em em-bicyclist" aria-role="presentation" aria-label="BICYCLIST"></i><i class="em em-ocean" aria-role="presentation" aria-label="WATER WAVE"></i>'
               },
               {
@@ -20,8 +20,8 @@ var main = new Vue({
                   picture: "images/profile.jpg"
                 },
                 description: "Construyendo la casa antes de que aparezcan los creepers",
-                comments: 0,
-                likes: 0,
+                comments: 12,
+                likes: 378,
                 emojis: '<i class="em em-crossed_swords" aria-role="presentation" aria-label=""></i><i class="em em-video_game" aria-role="presentation" aria-label="VIDEO GAME"></i>'
               },
               {
@@ -32,7 +32,7 @@ var main = new Vue({
                 },
                 description: "De chill tomando el sol",
                 comments: 0,
-                likes: 0,
+                likes: 89,
                 emojis: '<i class="em em-sunglasses" aria-role="presentation" aria-label="SMILING FACE WITH SUNGLASSES"></i><i class="em em-sunny" aria-role="presentation" aria-label="BLACK SUN WITH RAYS"></i>'
               },
               {
@@ -42,8 +42,8 @@ var main = new Vue({
                   picture: "images/progile.jpg"
                 },
                 description: "Una foto antes de que las olas se llevaran mis chanclas",
-                comments: 0,
-                likes: 0,
+                comments: 2,
+                likes: 160,
                 emojis: '<i class="em em-cry" aria-role="presentation" aria-label="CRYING FACE"></i>'
               },
               {
@@ -53,8 +53,8 @@ var main = new Vue({
                   picture: "images/profile.jpg"
                 },
                 description: "Modo incógnito",
-                comments: 0,
-                likes: 0,
+                comments: 46,
+                likes: 793,
                 emojis: '<i class="em em-sleuth_or_spy" aria-role="presentation" aria-label=""></i>'
               },
               {
@@ -64,8 +64,8 @@ var main = new Vue({
                   picture: "images/profile.jpg"
                 },
                 description: "Olvidé los audífonos en casa",
-                comments: 0,
-                likes: 0,
+                comments: 10,
+                likes: 790,
                 emojis: '<i class="em em-face_with_symbols_on_mouth" aria-role="presentation" aria-label="SERIOUS FACE WITH SYMBOLS COVERING MOUTH"></i>'
               }
         ],
