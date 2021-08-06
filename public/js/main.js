293d1a7111880deeb27b4dac955fdf8d24f97784
gabriel1925 <pocholo_1925@hotmail.com>
2021-08-06 09:23:06 -0300

    noche

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,63 @@
-let d = document
-window.addEventListener('DOMContentLoaded', (event) => {
-    let enlaces = Array.prototype.slice.apply(d.querySelectorAll('header nav .enlaces ul li'))
-    console.log(d.querySelector("header").clientHeight)
-    // console.log(enlaces)
-    enlaces.map((e,index)=>{
-        console.log(e)
-        e.addEventListener('click',(el)=>{
-            
-           console.log(e.offsetHeight )
-           
+let d = document;
+window.addEventListener("DOMContentLoaded", (event) => {
+  let secciones = Array.prototype.slice.apply(d.querySelectorAll("section"));
+  let links = Array.prototype.slice.apply(d.querySelectorAll("header nav ul li"));
+  let tamañoHeader = d.querySelector("header").clientHeight;
+  let tamañoSection = d.querySelector("section:nth-child(1)").offsetHeight;
+  let tamañoVentana =
+    tamañoHeader + tamañoSection * 6 + d.querySelector("footer").clientHeight;
+  let registroScroll;
+  let SumaTamaños;
+  links.map((e, index) => {
+      e.addEventListener("click", () => {
+        links.map((el)=>{
+            el.classList.remove('active')
         })
-    })
+        e.classList.add('active')
+      console.log(index);
+      ir(index);
+    });
+  });
+  function ir(index) {
+    if (index == 0) {
+        scroll({
+            top: 0,
+            behavior: "smooth"
+          });
+        
+    } else {
+      SumaTamaños = tamañoSection * index;
+      scroll({
+        top: SumaTamaños,
+        behavior: "smooth"
+      });
+    }
+  }
 
-
-// window.scrollTo(X,Y) mueve la pagina
-
-    // window.onscroll = function() {
-    //     console.log("Vertical: " + window.scrollY);
-    //     console.log("Horizontal: " + window.scrollX);
+  function Observador(entries) {
+    entries.map((e, index) => {
       
-    //   };
+      if (e.isIntersecting == true &&e.intersectionRatio>0.32 ) {
+        setTimeout(()=>{
+            e.target.classList.remove("nover");
+            e.target.classList.add("animacion");
+        },100)
+        //   observer.unobserve(e.target)
+      }else{
+        setTimeout(()=>{
+            e.target.classList.remove("animacion");
+            e.target.classList.add("nover");
+        },100)
+      }
+    });
+  }
+  let observer = new IntersectionObserver(Observador, { threshold: 0.3 });
+  secciones.map((e) => {
+    observer.observe(e);
+  });
 
-
+ 
+  
 
 
 
