538446fe6159d6b155226e62a87a5608c8db6e55
Alejandra <lilith.aglariment@gmail.com>
2021-08-06 17:42:30 -0500

    Cambios finales

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,14 @@
-let idU = 0;
-
-function imagen(id){
-    if (idU === id){
-         let fG = document.getElementById("imgGde").innerHTML = `<img src="${img}">`;
-    } else {
-        fG = '';
-        img = `images/${id}.jpg`;
-        fG = document.getElementById("imgGde").innerHTML = `<img src="${img}">`;
+if(screen.width(500)){
+    let idU = 0;
+    function imagen(id){
+        if (idU === id){
+            let fG = document.getElementById("imgGde").innerHTML = `<img src="${img}">`;
+        } else {
+            fG = '';
+            img = `images/${id}.jpg`;
+            fG = document.getElementById("imgGde").innerHTML = `<img src="${img}">`;
+        }
+        idU = id;
+        
     }
-    idU = id;
-    
-}
+}  
