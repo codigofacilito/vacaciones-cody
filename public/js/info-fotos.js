430677cc7f11722deb73bcc13930cb774c7a126d
GabrielTenZig <jesusgabrieltenorioziga@gmail.com>
2021-08-06 23:38:40 -0500

    Termine pagina INICIO y tambien pagina FOTOS (TERMINADO).
    - Se agrego diseño a las secciones restantes en INICIO
    - Se agrego un pie de pagina
    - Se le dio diseño a la secicon de fotos
    - Se agrego un modal para ver las fotos
    - Se agrego la funcionalidad de dar "Me encanta"
    - Se hico el menu Hamburguesa para las versiones moviles
    - Se minificaron las imagenes.

---
+++ public/js/info-fotos.js
@@ -0,0 +1,46 @@
+const info_fotos = [
+    {
+        url: "./images/galeria/1.jpg",
+        id: "001",
+        titulo: "Montando en bicicleta",
+        descripcion: "Recuerdo ese dia, estaba caminando por una playa del caribe, cuando vi un local donde rentaban bicicletas, no lo pense y rente una, esa fotografia la tomó un amable turista, fue un gran día.",
+        ubicacion: "Playa Cana, República Dominicana."
+    },
+    {
+        url: "./images/galeria/2.jpg",
+        id: "002",
+        titulo: "Acampando en la playa",
+        descripcion: "Este fué un día increíble, después de recorrer las playas de Puerto Escondido, decidi acampar en una de ellas, definitivamente volvere allí.",
+        ubicacion: "Playa Manzanillo, Puerto Escondido, Oaxaca, México."
+    },
+    {
+        url: "./images/galeria/3.jpg",
+        id: "003",
+        titulo: "Relajandome en la alberca",
+        descripcion: "Al llegar a Cancún, decidí darme un lujo y descansar en un hotel con alberca, recuerdo que mas tarde opte por estudiar un poco de frontend, aprendí mucho mejor después de un descanso.",
+        ubicacion: "Cancún, México."
+    },
+    {
+        url: "./images/galeria/4.jpg",
+        id: "004",
+        titulo: "Relajándome en la playa",
+        descripcion: "Esta foto también es de Puerto Escondido, es relajante sentarse a ver el atardecer a la orilla de la playa.",
+        ubicacion: "Playa Zicatela, Puerto Escondido, Oaxaca, México."
+    },
+    {
+        url: "./images/galeria/5.jpg",
+        id: "005",
+        titulo: "Recorriendo Guanajuato",
+        descripcion: "Guanajuato es conocido por sus cajones, recuerdo que ese día buscaba el famoso Callejón del beso, pero me perdí entre tanto callejón.",
+        ubicacion: "Guanajuato, México."
+    },
+    {
+        url: "./images/galeria/6.jpg",
+        id: "006",
+        titulo: "Un mal día",
+        descripcion: "Cuando estaba por volver a casa, informaron que el vuelo se retrasaría 10 horas, y tenia que volver para hacer un pull request del proyecto que dejé en mi repositorio local de mi PC.",
+        ubicacion: "Aeropuerto, en algún lugar del mundo."
+    }
+] 
+
+export { info_fotos }
