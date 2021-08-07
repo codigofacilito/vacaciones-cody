1d5f5be10dfa9e1a7c8a24fde3d80a25d1c0bd08
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2021-08-06 19:46:48 -0500

    Interacción con javascript agregada

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,88 @@
-const menuIcons = document.querySelectorAll('.menu-toggle');
-const navbar = document.querySelector('.navbar-menu')
+const body = document.querySelector('body');
 
+const menuIcons = document.querySelectorAll('.menu-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+const navbar = document.querySelector('.navbar');
+const hero = document.querySelector('.hero-content');
+
+const rowUp = document.querySelector('.row-up');
+
+
+const btnsVerFoto = document.querySelectorAll('.btn-zoom');
+
+btnsVerFoto.forEach( btnVerFoto =>{
+    btnVerFoto.addEventListener('click', ()=>{
+        const foto = btnVerFoto.parentNode.parentNode.childNodes[1];
+        mostrarFoto(foto);
+    });
+} );
+
+
+
+// Menu responsive
 menuIcons.forEach( menuIcon =>{
-    console.log(menuIcon);
     menuIcon.addEventListener('click', ()=>{
-        navbar.classList.toggle('menu-visible');
+        navbarMenu.classList.toggle('menu-visible');
     });
 });
+
+// Ir arriba
+rowUp.addEventListener('click', ()=>{
+    hero.style.marginTop = `0px`;
+});
+
+
+// Escuchando al Scroll
+document.addEventListener('scroll', ()=>{
+    navbarSticky();
+});
+
+// Navbar fijo
+function navbarSticky(){
+    const observer = new IntersectionObserver((entries)=>{
+
+        if( entries[0].boundingClientRect.top <= 32 ){
+            navbar.classList.add('navbar-sticky');
+            hero.style.marginTop = `${navbar.offsetHeight}px`;     
+            rowUp.classList.add('row-visible');
+        }
+        else if( entries[0].boundingClientRect.top >= 0 ){
+            navbar.classList.remove('navbar-sticky');
+            hero.style.marginTop = '0';        
+            rowUp.classList.remove('row-visible');
+        }
+    });
+    observer.observe( hero );
+}
+
+function mostrarFoto(e){
+    console.log(e.src);
+    
+    const foto = document.createElement('IMG');
+    foto.src = e.src;
+
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(foto);
+    overlay.classList.add('overlay');
+    overlay.onclick = ()=>{
+        overlay.remove();
+        body.classList.remove('fijar-body');
+    }
+
+    // Cerrar foto
+    const cerrarFoto = document.createElement('P');
+    cerrarFoto.textContent = 'X';
+    cerrarFoto.classList.add('btn-cerrar');
+    overlay.appendChild(cerrarFoto);
+
+    cerrarFoto.onclick = ()=>{
+        overlay.remove();
+        body.classList.remove('fijar-body');
+    }
+
+    // Mostrar en HTML
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+}
+
